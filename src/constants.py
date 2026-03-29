8c89c81eeb53607ed56b562df8f1f5b598d0dfeb
Roman Ponomarenko <ponomarenko.roman@gmail.com>
2026-03-29 08:35:02 +0300

    Add login fixtures, refactor tests, and enhance config management

---
+++ src/constants.py
@@ -0,0 +1,7 @@
+from dataclasses import dataclass
+
+
+@dataclass(frozen=True)
+class PageTitles:
+    HP_TITLE = "AI Test Management Tool | Testomat.io"
+    SIGNIN_TITLE = "Testomat.io"
---
+++ tests/conftest.py
@@ -0,0 +1,8 @@
+import pytest
+
+from faker import Faker
+
+
+@pytest.fixture(scope="session")
+def faker():
+    return Faker()
